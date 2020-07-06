6a24acaf5185dc5df4d9dd5925de4b6e2c0f42fb
MusaTamzid05 <tamzid101@gmail.com>
2020-07-07 05:09:14 +0600

    converting to smaller clip added.

--- editor/editor.go
+++ editor/editor.go
@@ -1,13 +1,21 @@
 package editor
 
-import "fmt"
+import (
+	"fmt"
+	"video_editor/data"
+	"video_editor/util"
+)
 
 type Editor struct {
 	path string
 }
 
-func (e *Editor) Process() {
-	fmt.Println("Basic editor struct")
+func (e *Editor) ExtractClip(output string, startTime data.Time, duration int) error {
+
+	commandStr := fmt.Sprintf("ffmpeg -i %s -ss %s -codec copy -t %d %s", e.path, startTime.String(), duration, output)
+	fmt.Println(commandStr)
+	_, err := util.ExecuteCommand(commandStr)
+	return err
 }
 
 func NewEditor(path string) *Editor {
--- video_editor.go
+++ video_editor.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"video_editor/data"
+	"video_editor/editor"
 )
 
 func main() {
-	t1 := data.NewTime(6, 5, 7)
-	fmt.Println(t1.String())
-
-	t2 := data.NewTime(11, 12, 13)
-	fmt.Println(t2.String())
+	editor := editor.NewEditor("/home/musa/Downloads/lifeboy.mp4")
+	editor.ExtractClip("test.mp4", *data.NewTime(0, 0, 5), 10)
 }
