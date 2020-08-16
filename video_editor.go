6ab22e7bf23f7583e1b19223daad8ea89bde2541
MusaTamzid05 <tamzid101@gmail.com>
2020-08-16 23:51:24 +0600

    flag added for splitting

--- video_editor.go
+++ video_editor.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"video_editor/editor"
 )
 
@@ -14,7 +16,30 @@ func SplitVideoFrom(videoPath, timeJsonPath, dstPath string) {
 	}
 }
 
+func Usage() {
+	log.Fatalln("Usage: -s -v path -j json_path -d dst.mp4")
+}
+
 func main() {
-	SplitVideoFrom("/home/musa/Downloads/lifeboy.mp4", "data.json", "final.mp4")
+
+	splitPtr := flag.Bool("s", false, "Set it to false")
+	videoSrcPath := flag.String("v", "", "Video src path")
+	jsonPathPtr := flag.String("j", "", "Json path")
+	dstPathPtr := flag.String("d", "", "Dst path")
+
+	flag.Parse()
+
+	if *videoSrcPath == "" {
+		Usage()
+	}
+
+	if *splitPtr == true {
+		if *jsonPathPtr == "" || *dstPathPtr == "" {
+			Usage()
+		}
+		SplitVideoFrom(*videoSrcPath, *jsonPathPtr, *dstPathPtr)
+	}
+
+	//SplitVideoFrom("/home/musa/Downloads/lifeboy.mp4", "data.json", "final.mp4")
 
 }
